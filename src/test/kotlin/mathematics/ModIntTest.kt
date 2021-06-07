4ab4e5c27cffdede7ee54e3a68cfe3609b8420bc
mban <mban259@users.noreply.github.com>
2021-06-07 23:26:40 +0900

    追加:ModIntTest plus1, plus2

--- src/test/kotlin/mathematics/ModIntTest.kt
+++ src/test/kotlin/mathematics/ModIntTest.kt
@@ -5,5 +5,17 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class ModIntTest {
+    @Test
+    fun plus1() {
+        val mint1 = ModInt(10)
+        val mint2 = mint1 + 1000000000
+        assertEquals(3, mint2.num)
+    }
 
+    @Test
+    fun plus2() {
+        val mint1 = ModInt(10)
+        val mint2 = mint1 + 100000000000000
+        assertEquals(100000000000010 % 1000000007, mint2.num)
+    }
 }
